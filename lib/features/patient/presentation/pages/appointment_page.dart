aad430565b15beafcd0c2f773a1fe61741579330
DerekLC08 <derek.leechung6@gmail.com>
2024-07-21 15:03:36 +0400

    Appointment page

--- lib/features/patient/presentation/pages/appointment_page.dart
+++ lib/features/patient/presentation/pages/appointment_page.dart
@@ -7,13 +7,65 @@ class AppointmentPage extends StatefulWidget {
   State<AppointmentPage> createState() => _AppointmentPageState();
 }
 
+//Block to connect Date and Description box
+//class to fetch and store data in the class
+//call class to display data
+
+/*class _Displaytab extends State<AppointmentPage> {
+  @override
+}*/
+
 class _AppointmentPageState extends State<AppointmentPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Appointment Page'),
+    return Scaffold(
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 200,
+              color: Colors.deepPurple[50],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 200,
+              color: Colors.deepPurple[50],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 200,
+              color: Colors.deepPurple[50],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 200,
+              color: Colors.deepPurple[50],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 200,
+              color: Colors.deepPurple[50],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              height: 200,
+              color: Colors.deepPurple[50],
+            ),
+          ),
+        ]
       ),
     );
   }
 }
+
