a143039e21c11f651f4cc93435978ea24a4f8924
JoottunAtish <ajoottun24@gmail.com>
2024-07-23 21:52:11 +0400

    Adding gemini star logo

--- lib/features/core/gemini/gemini.dart
+++ lib/features/core/gemini/gemini.dart
@@ -1,20 +1,15 @@
 import 'package:flutter_gemini/flutter_gemini.dart';
-import 'dart:io';
 
 class GeminiIntergace {
-  String imgPath;
   String textIn;
 
   final gemini = Gemini.instance;
 
-  GeminiIntergace(this.imgPath, this.textIn);
+  GeminiIntergace( this.textIn);
 
-  String getOutput() {
-    final file = File(imgPath);
-    String output = "";
-    gemini
-        .textAndImage(text: textIn, images: [file.readAsBytesSync()])
-        .then((value) => (output = (value?.content?.parts?.last.text ?? '')));
+  String? getOutput(){
+    String? output = "";
+    gemini.text(textIn).then((value) => (output = (value?.output)));
     return output;
   }
 }
