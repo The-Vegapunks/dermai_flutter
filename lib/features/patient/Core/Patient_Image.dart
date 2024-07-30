6fae448d40bb26d088d062e2e759b8357536dd9a
JoottunAtish <ajoottun24@gmail.com>
2024-07-30 17:05:18 +0400

    addin the return parameter

--- lib/features/patient/Core/Patient_Image.dart
+++ lib/features/patient/Core/Patient_Image.dart
@@ -4,7 +4,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
 
-
 class PatientImage {
   late File imgPath;
   final picker = ImagePicker();
@@ -21,18 +20,20 @@ class PatientImage {
     imgPath = File(image!.path);
   }
 
-  void sendImg(String Filename) async {
+  Future<String?> sendImg(String Filename) async {
     // send request to python flask server
     var postURi = Uri.parse("URi here");
 
     var request = http.MultipartRequest("POST", postURi);
 
-    request.files.add(http.MultipartFile.fromBytes(
+    http.Response res = await http.Response.fromStream(request.files.add(http.MultipartFile.fromBytes(
         Filename, File.fromRawPath(imgPath.readAsBytesSync()).readAsBytesSync(),
         contentType: MediaType('image', 'jpeg')));
 
-        request.send().then((response){
-          if (response.statusCode == 200) print("uploaded!!");
-        });
+    request.send().then((response) {
+      if (response.statusCode == 200) print("uploaded!!");
+    }););
+
+    return res.headers['response'];
   }
 }
