1d6a35b0a8819251062d4748d4764dc9df5f9015
DerekLC08 <derek.leechung6@gmail.com>
2024-08-06 12:05:03 +0400

    OTP and Reset password front end fully functional

--- lib/features/auth/presentation/pages/fp_reset_password_page.dart
+++ lib/features/auth/presentation/pages/fp_reset_password_page.dart
@@ -1,33 +1,70 @@
+import 'package:dermai/features/auth/presentation/pages/sign_in_page.dart';
 import 'package:flutter/material.dart';
 
-class FpResetPasswordPage extends StatefulWidget {
-  final String email;
-  const FpResetPasswordPage({super.key, required this.email});
+class FpResetPasswordPage extends StatelessWidget {
+  final TextEditingController newPasswordController = TextEditingController();
+  final TextEditingController confirmNewPasswordController = TextEditingController();
 
-  @override
-  State<FpResetPasswordPage> createState() => _FpResetPasswordPageState();
-}
+  FpResetPasswordPage({super.key});
 
-class _FpResetPasswordPageState extends State<FpResetPasswordPage> {
   @override
   Widget build(BuildContext context) {
-    final String email = widget.email;
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Reset Password'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: const Text('Change Password'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text('Reset Password for $email'),
-            const SizedBox(height: 20),
+            TextField(
+              controller: newPasswordController,
+              decoration: const InputDecoration(
+                labelText: 'New Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: confirmNewPasswordController,
+              decoration: const InputDecoration(
+                labelText: 'Confirm New Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 32.0),
             ElevatedButton(
               onPressed: () {
-                // Implement reset password logic here
+                if (newPasswordController.text == confirmNewPasswordController.text) {
+                  // Call to change password use case
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Password changed successfully')),
+                  );
+                  // Implement change in supabase
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                    builder: (context) => const SignInPage()));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Passwords do not match')),
+                  );
+                }
               },
-              child: const Text('Reset Password'),
+              child: const Text('Change Password'),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+ 
+              ),
             ),
           ],
         ),
@@ -35,3 +72,6 @@ class _FpResetPasswordPageState extends State<FpResetPasswordPage> {
     );
   }
 }
+
+
+
